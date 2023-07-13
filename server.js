0c740e6018807a6c97c6ddd0198785ee779f3a28
Jayden Schuffenhauer <jdrich135@gmail.com>
2023-07-12 19:25:10 -0600

    finished setup

--- server.js
+++ server.js
@@ -3,8 +3,8 @@ const express = require('express');
 const session = require('express-session');
 const exphbs = require('express-handlebars');
 const routes = require('./controllers');
-const helpers = require('./utils/helpers');
 const bodyParser = require('body-parser');
+const helpers = require('./utils/helpers');
 
 const sequelize = require('./config/connection');
 
