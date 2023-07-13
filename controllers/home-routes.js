1fb8102e2aeee444f689d40f2a5c47dc8481be25
Jayden Schuffenhauer <jdrich135@gmail.com>
2023-07-12 19:10:57 -0600

    added schema

--- controllers/home-routes.js
+++ controllers/home-routes.js
@@ -1,5 +1,4 @@
 const router = require('express').Router();
-const { User, Character, Race, Class, Background, Monster, Spell } = require('../models');
 const withAuth = require('../utils/auth');
 
 
